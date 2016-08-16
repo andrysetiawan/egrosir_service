61cc073e8151ce0d945775a7752dd70d337f604c
(andrysetiawan) <(yustinusandrys@gmail.com)>
2016-08-16 12:41:41 +0700

    Commit egrosir service project

---
+++ README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+# Egrosir REST API
+=======
+# Egrosir REST API
+>>>>>>> 750053bc3204760ff3c6ca337daccc880c5b60f5
---
+++ src/routes.php
@@ -0,0 +1,21 @@
+<?php
+// Routes
+
+// $app->get('/[{name}]', function ($request, $response, $args) {
+//     // Sample log message
+//     $this->logger->info("Slim-Skeleton '/' route");
+
+//     // Render index view
+//     return $this->renderer->render($response, 'index.phtml', $args);
+// });
+
+//Auth Routes
+$app->post('/auth/signup','auth_controller:sign_up');
+$app->post('/auth/signin','auth_controller:sign_in');
+
+//User Routes
+$app->get('/user','user_controller:get_all');
+$app->get('/user/{id}','user_controller:get_by_id');
+$app->post('/user/add','user_controller:insert');
+$app->put('/user/update/{id}','user_controller:update');
+$app->delete('/user/delete/{id}','user_controller:delete');
