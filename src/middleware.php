de7f49af4f1bdcd00d08b768aee27146a0757e9b
(andrysetiawan) <(yustinusandrys@gmail.com)>
2016-08-23 21:07:08 +0700

    add JWT middleware

--- src/middleware.php
+++ src/middleware.php
@@ -1,4 +1,41 @@
 <?php
 // Application middleware
+use App\Controllers\Token;
+use Slim\Middleware\JwtAuthentication;
+use Slim\Middleware\HttpBasicAuthentication;
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+$container = $app->getContainer();
+$container["token"] = function ($container) {
+    return new Token;
+};
+
+$container["JwtAuthentication"] = function ($container) {
+    return new JwtAuthentication([
+        "path" => ["/user","/admin",
+        		   "/pasar/add","/pasar/update/","/pasar/delete/",
+        		   "/kategori/add","/kategori/update/","kategori/delete/",
+        ],
+        "passthrough" => ["/auth/signin"],
+        "secret" => "hatepnganuikihihamburadul",
+        "secure" => false,
+        "error" => function ($request, $response, $arguments) {
+            $data["status"] = "error";
+            $data["message"] = $arguments["message"];
+            return $response
+                ->withHeader("Content-Type", "application/json")
+                ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+        },
+        "callback" => function ($request, $response, $arguments) use ($container) {
+            $container["token"]->hydrate($arguments["decoded"]);
+        }
+    ]);
+};
+
+//$app->add("HttpBasicAuthentication");
+$app->add("JwtAuthentication");
+
+
+$container["cache"] = function ($container) {
+    return new CacheUtil;
+};
