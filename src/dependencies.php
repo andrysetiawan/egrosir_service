e7fc1be9ce11fefcd81b95a1e6fdb4205bc3f2a0
(andrysetiawan) <(yustinusandrys@gmail.com)>
2016-08-23 21:06:17 +0700

    initialization new controllers

--- src/dependencies.php
+++ src/dependencies.php
@@ -34,12 +34,27 @@ $container['validator'] = function($container) {
 $container['hash_password'] = function($container){
     return new \App\Controllers\hash_password;
 };
+$container['Token'] = function($container){
+    return new \App\Controllers\Token;
+};
+$container['admin_controller'] = function($container) {
+    return new \App\Controllers\admin_controller($container);
+};
 $container['user_controller'] = function($container) {
     return new \App\Controllers\user_controller($container);
+};
+$container['admin_auth_controller'] = function($container) {
+    return new \App\Controllers\admin_auth_controller($container);
 };
 $container['auth_controller'] = function($container) {
     return new \App\Controllers\auth_controller($container);
 };
 $container['barang_controller'] = function($container) {
     return new \App\Controllers\barang_controller($container);
+};
+$container['kategori_controller'] = function($container) {
+    return new \App\Controllers\kategori_controller($container);
+};
+$container['pasar_controller'] = function($container) {
+    return new \App\Controllers\pasar_controller($container);
 };
